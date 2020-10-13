0444733a456bbc31aab4b6ebb2fa2eae6d76603f
punavwalke <punavw17@gmail.com>
2020-10-13 21:51:42 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,102 @@
+export const storeProducts=[
+    {
+    id: 1,
+    title: "Chef's Choice CLASSIC Cupcake 6 Box",
+    img: "img/img1.jpg",
+    price: 20,
+    info:
+      "This is the perfect collection for the person who wants us to select a variety of classic flavors for you.  Our Chef will create your collection of 6 assorted flavors from a list of traditional and unique artisanal flavor combinations from our current menu.  Leave the selection to us and we will put together a delicious assortment for you.  It's that easy.",
+    inCart: false,
+    count: 0,
+    total: 0
+    },
+    {
+      id: 2,
+      title: "September Cupcake Calendar",
+      img: "img/img2.jpg",
+      price: 16,
+      info:
+        "Pumpkin Spice: This cake or cupcake is made with pumpkin, dried cranberries and aromatic spices that enhance this delicious cake. It is iced with caramel cream cheese. Red Velvet cake with whipped vanilla icing. Every day we serve our classic cupcakes!",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 3,
+      title: "Earl Grey ",
+      img: "img/img3.jpg",
+      price: 30,
+      company: "htc",
+      info:
+        "This tea inspired cupcake and buttercream are both made with loose-leaf Earl Grey tea, giving each bite that beloved Earl Grey bergamot flavor.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+        id: 4,
+        title: "Vanilla-Strawberry Delicacy",
+        img: "img/img4.jpg",
+        price: 40,
+        info:
+          "Enticing look, super delicious taste and best in class decoration, these are the elements that describe this cake in a perfect manner. This cup cake can easily still anyone's heart with its perfect taste and design. Make this eve of Christmas really memorable by ordering this decadent cup cake with a highly delectable taste. It comes with a fondant made rose flower top up. It is a set of 6 cupcakes.",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+    id: 5,
+    title: "Charming choco-vanilla cupcake",
+    img: "img/img5.jpg",
+    price: 25,
+    info:
+      "Wow' is the only word that naturally comes out of your mouth after seeing this cute sugary delight. Coming with chocolate cup cake base, whipped vanilla cream, chocolate sauce and the sprinkles of edible sugar candies, this set of 6 cup cake can easily hold a permanent space in anyone's heart.",
+    inCart: false,
+    count: 0,
+    total: 0
+    },
+    {
+    id: 6,
+    title: "Radiant Red Velvet Cupcake",
+    img: "img/img6.jpg",
+    price: 30,
+    info:
+      "This delicious red velvet and cream cup cake is the ideal treat for the people who always love something unique. People not only love these cakes for their delicious taste of cream and cake but they look nice also. Each piece of this cake can really help you to reach the highest peak of dessert joy.",
+    inCart: false,
+    count: 0,
+    total: 0
+    },
+    {
+    id: 7,
+    title: "Unicorn Cupcakes",
+    img: "img/img7.jpg",
+    price: 60,
+    info:
+    "Take yourself to the magical world of unicorns and live the fantasy by eating these tasty unicorn cupcakes. ",
+    inCart: false,
+    count: 0,
+    total: 0
+    },
+    {
+    id: 8,
+    title: "Mint Chocolate Oreo Cupcakes",
+    img: "img/img8.jpg",
+    price: 35,
+    info:
+      "Mint Chocolate cupcake filled with dark chocolate ganache and Oreo cream frosting.",
+    inCart: false,
+    count: 0,
+    total: 0
+    }
+]
+export const detailProduct={
+    id: 1,
+    title: "Chef's Choice CLASSIC Cupcake 6 Box",
+    img: "img/img1.jpg",
+    price: 20,
+    info:
+      "This is the perfect collection for the person who wants us to select a variety of classic flavors for you.  Our Chef will create your collection of 6 assorted flavors from a list of traditional and unique artisanal flavor combinations from our current menu.  Leave the selection to us and we will put together a delicious assortment for you.  It's that easy.",
+    inCart: false,
+    count: 0,
+    total: 0
+}
